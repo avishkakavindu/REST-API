b4f0e0997e21657ee038a16c457c38de86b8520a
avishkakavindu <avishkakavindud@gmail.com>
2020-11-07 20:26:41 +0530

    Login view added

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from knox import views as knox_views
 from . import views
 
 urlpatterns = [
-    path('register/', views.Register.as_view(), name='register'),
+    path('register/', views.RegisterApi.as_view(), name='register'),
+    path('login/', views.LoginApi.as_view(), name='login'),
+    path('logout/', knox_views.LogoutView.as_view(), name=''),
 ]
