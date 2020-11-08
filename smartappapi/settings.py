a7bd6221b2600c9c022cc6a71a88cd77c31112a1
avishkakavindu <avishkakavindud@gmail.com>
2020-11-08 20:36:35 +0530

    ENV added

--- smartappapi/settings.py
+++ smartappapi/settings.py
@@ -140,5 +140,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = email_credentials['username']
-EMAIL_HOST_PASSWORD = email_credentials['password']
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+print(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
