b2acbecbd695282a04ed59c4fba6a34d3548797c
avishkakavindu <avishkakavindud@gmail.com>
2020-11-08 09:55:22 +0530

    Abstract models added

--- authentication/models.py
+++ authentication/models.py
@@ -1,1 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if username is None:
+            raise TypeError('User need to have username!')
+        if email is None:
+            raise TypeError('User need to have Email!')
+
+        user =  self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+
+    def create_superuser(self, username, email, password):
+        if password is None:
+            raise TypeError("Password shouldn't be None!")
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True, db_index=True)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    is_verified = models.BooleanField(default=False)        # email verified?
+    is_active = models.BooleanField(default=True)           # is user active?
+    is_staff = models.BooleanField(default=False)           # is user in staff?
+    created_at = models.DateTimeField(auto_now_add=True)    # registered date
+    updated_at = models.DateTimeField(auto_now_add=True)    # last update date
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
--- authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from knox import views as knox_views
-from . import views
-
-urlpatterns = [
-    path('register/', views.RegisterApi.as_view(), name='register'),
-    path('login/', views.LoginApi.as_view(), name='login'),
-    path('logout/', knox_views.LogoutView.as_view(), name=''),
-]
